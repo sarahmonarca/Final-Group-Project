6471edc73e47aff4e5a67a6d1149b25712669291
NickyPiscitelli <112560950+NickyPiscitelli@users.noreply.github.com>
2022-12-11 20:22:12 -0500

    Update ORDERED FINAL PROJECT.py

--- ORDERED FINAL PROJECT.py
+++ ORDERED FINAL PROJECT.py
@@ -149,6 +149,45 @@ finally:
     GPIO.cleanup()
 ##############PREASSURE SENSOR
 ##############RIDDLE 2
+noww = strftime("THIRD RIDDLE")
+
+
+root2=Tk()
+root2.title("BATMAN VS THE RIDDLER")
+canvas= Canvas(root2, width = 350, height=250)
+canvas.pack()
+#CAN ADD A DIFFERENT PICTURE BELOW IF WANT
+photo=PhotoImage(file='riddler.gif') 
+canvas.create_image(0, 0, anchor = NW, image=photo)
+
+tkinter.messagebox.showinfo("RIDDLES", noww)
+
+namea = tkinter.simpledialog.askstring("??", "Riddle: I can be cracked, I can be made, I can be told, and I can be played. What am I" )
+
+# try creating new root names for each pop up and then closing them for the next one to be created
+B = 3
+while (B > 0):
+    if (namea != "A joke"):
+        if(B == 0):
+            break
+        else:
+            print(B, "more trys")
+            B = B - 1
+            namea = tkinter.simpledialog.askstring("??", "I can be cracked, I can be made, I can be told, and I can be played. What am I ?" )
+            #DETERMINE WHAT WILL HAPPEN IF C RUNS OUT
+    elif(namea == "A joke"):
+        root2 = Tk()
+        root2.title("Main")
+        root2.geometry("900x500")
+        global e1
+        global e2
+        Label(root2, text = "Congratulations BATMAN. You have completed my simplest riddle. Time to step it up. You are about to play what I like ot calls the PREASSURE RIDDLE. \n A little hint to help you solve this. Find the right preassure HAHA GOODLUCK BATMAN").place(x=5, y=5)
+        B = 0
+    
+
+root2.mainloop()
+time.sleep(1)
+
 ##############RIDDLE 2
 ##############BEEP COLOR
 import RPi.GPIO as GPIO
@@ -283,6 +322,45 @@ except KeyboardInterrupt:
     print("\nBbye")
 ##############BEEP COLOR
 ##############RIDDLE 3
+noww = strftime("THIRD RIDDLE")
+
+
+root9=Tk()
+root9.title("BATMAN VS THE RIDDLER")
+canvas= Canvas(root9, width = 350, height=250)
+canvas.pack()
+#CAN ADD A DIFFERENT PICTURE BELOW IF WANT
+photo=PhotoImage(file='riddler.gif') 
+canvas.create_image(0, 0, anchor = NW, image=photo)
+
+tkinter.messagebox.showinfo("RIDDLES", noww)
+
+nameq = tkinter.simpledialog.askstring("??", "Riddle: I can be cracked, I can be made, I can be told, and I can be played. What am I" )
+
+# try creating new root names for each pop up and then closing them for the next one to be created
+Z = 3
+while (Z > 0):
+    if (nameq != "A joke"):
+        if(Z == 0):
+            break
+        else:
+            print(Z, "more trys")
+            Z = Z - 1
+            nameq = tkinter.simpledialog.askstring("??", "I can be cracked, I can be made, I can be told, and I can be played. What am I ?" )
+            #DETERMINE WHAT WILL HAPPEN IF C RUNS OUT
+    elif(nameq == "A joke"):
+        root9 = Tk()
+        root9.title("Main")
+        root9.geometry("900x500")
+        global e1
+        global e2
+        Label(root9, text = "Congratulations BATMAN. You have completed my simplest riddle. Time to step it up. You are about to play what I like ot calls the PREASSURE RIDDLE. \n A little hint to help you solve this. Find the right preassure HAHA GOODLUCK BATMAN").place(x=5, y=5)
+        Z = 0
+    
+
+root9.mainloop()
+time.sleep(1)
+
 ##############RIDDLE 3
 ##############DISTANCE SENSOR
 #RANGE SENSOR CODE
@@ -365,6 +443,84 @@ GPIO.cleanup()
 distances = []
 
 ##############DISTANCE SENSOR
+##############GUESSING GAME
+from tkinter import *
+import random
+import sys
+from tkinter import Tk, Label, Button, Entry, StringVar, DISABLED, NORMAL, END, W, E
+
+class GuessingGame:
+    def __init__(self, master):
+        self.master = master
+        master.title("Guessing Game")
+
+        self.secret_number = random.randint(1, 50)
+        self.guess = None
+        self.num_guesses = 0
+
+        self.message = "OH NO, The riddler got away. The only way to find him is to guess what building he went into! Quick, the apartment numbers range from 1-50! "
+        self.label_text = StringVar()
+        self.label_text.set(self.message)
+        self.label = Label(master, textvariable=self.label_text, foreground = "black", background = "white")
+        vcmd = master.register(self.validate) # we have to wrap the command
+        self.entry = Entry(master, validate="key", validatecommand=(vcmd, '%P'))
+        self.guess_button = Button(master, text="Guess", command=self.guess_number)
+        self.reset_button = Button(master, text="Enter Building", command=self.quit_game, state=DISABLED)
+        self.label.grid(row=0, column=0, columnspan=2, sticky=W+E)
+        self.entry.grid(row=1, column=0, columnspan=2, sticky=W+E)
+        self.guess_button.grid(row=2, column=0)
+        self.reset_button.grid(row=2, column=1)
+
+    def validate(self, new_text):
+        if not new_text: # the field is being cleared
+            self.guess = None
+            return True
+        try:
+            guess = int(new_text)
+            if 1 <= guess <= 50:
+                self.guess = guess
+                return True
+            else:
+                return False
+        except ValueError:
+            return False
+
+    def guess_number(self):
+        self.num_guesses += 1
+        if self.guess is None:
+            self.message = "Guess a number from 1 to 50"
+        elif self.guess == self.secret_number:
+            suffix = '' if self.num_guesses == 1 else 'es'
+            self.message = "Congratulations! You guessed the number after %d guess%s." % (self.num_guesses, suffix)
+            self.guess_button.configure(state=DISABLED)
+            self.reset_button.configure(state=NORMAL)
+        elif self.guess < self.secret_number:
+            self.message = "Too low! Guess again!"
+        else:
+            self.message = "Too high! Guess again!"
+
+        self.label_text.set(self.message)
+  
+    def quit_game(self):
+        root5 = Tk()
+        root5.title("Main")
+        root5.geometry("500x500")
+        global e1
+        global e2
+        root.destroy()
+
+        Label(root5, text = " Dear Batman, \n \n \n I see the woman has told you about her son, \n I knew it would get your attention!. \n So heres the deal Batsy, you complete my riddles, and I will set the boy free. \n Sounds pretty fare doesnt it? \n There are seven riddles that progressively will get harder. \n Once you have completed all of them, \n the boy will be set free. We will start off simply \n ARE YOU READY \n \n \n \n FROM ENIGMA AKA RIDDLER").place(x=5, y=5)
+        root5.mainloop()
+        time.sleep(1)
+        GPIO.cleanup()
+        sys.exit()
+
+root = Tk()
+my_gui = GuessingGame(root)
+root.mainloop()
+
+
+##############GUESSING GAME
 ##############ROOM GAME
 keys =[]
 VERBS = [ "go", "look", "take" ]
@@ -779,3 +935,4 @@ g = Game(window)
 g.play()
 window.mainloop()
 ##############ROOM GAME
+
